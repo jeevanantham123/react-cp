40d840cfbb5f8386380c3f36951e41b483463dca
jeevanantham <51525786+jeevanantham123@users.noreply.github.com>
2021-11-24 12:00:41 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { SketchPicker } from "./Sketch";
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [color, setcolor] = useState("#000");
+  const [state, setState] = useState();
+  useEffect(() => {
+    setState({
+      h: 150,
+      s: 0.5,
+      l: 0.2,
+      a: 1,
+    });
+  }, []);
+  const handleChangeComplete = (data) => {
+    // console.log(data);
+    if (data.hsl !== state) {
+      setState(data.hsl);
+    }
+
+    setcolor(data.hsl);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SketchPicker
+        color={color}
+        onChange={handleChangeComplete}
+        onChangeComplete={handleChangeComplete}
+      />
     </div>
   );
 }
-
-export default App;
